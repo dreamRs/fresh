7375851a3b771120cccb8cd5193a856503491df7
pvictor <perrier.victor@gmail.com>
2019-10-14 19:12:17 +0200

    package doc

---
+++ R/fresh-package.R
@@ -0,0 +1,8 @@
+#' Fresh 'Shiny' Themes
+#'
+#' Customize 'Bootstrap' and 'Bootswatch' themes, like colors, fonts, grid layout, to use in 'Shiny' applications.
+#'
+#' @name fresh
+#' @docType package
+#' @author Victor Perrier & Fanny Meyer (\href{https://twitter.com/dreamRs_fr}{@@dreamRs_fr})
+NULL
